cc786843f49fe5f2aff24afe10ffb5845d79e08f
Konstantin Plis <plis.konstantin@gmail.com>
2025-07-22 07:56:11 +0800

    feat(byoc gcp): add required roles

--- locals.tf
+++ locals.tf
@@ -1,14 +1,18 @@
 locals {
   clickhouse_sa_roles = {
-    network_admin = {
+    compute_network_admin = {
       # Manage VPC
       role = "roles/compute.networkAdmin"
     }
-    service_account_admin = {
+    iam_service_account_admin = {
       # Manage SA
       role = "roles/iam.serviceAccountAdmin"
     }
-    service_account_user = {
+    iam_service_account_key_admin = {
+      # Manage SA keys
+      role = "roles/iam.serviceAccountKeyAdmin"
+    }
+    iam_service_account_user = {
       # We need iam.serviceAccounts.actAs permission to deploy GKE
       role = "roles/iam.serviceAccountUser"
     }
@@ -19,6 +23,10 @@ locals {
     container_admin = {
       # Manage GKE
       role = "roles/container.admin"
+    }
+    resourcemanager_project_iam_admin = {
+      # Required to grant roles/container.defaultNodeServiceAccount to nodePool SA
+      role = "roles/resourcemanager.projectIamAdmin"
     }
   }
 }
