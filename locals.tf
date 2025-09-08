e8414d6c8ee92317de79f0431ebeff633b138568
Konstantin Plis <plis.konstantin@gmail.com>
2025-09-08 16:47:48 +0800

    feat(roles): use custom IAM roles for Clickhouse Management SA

--- locals.tf
+++ locals.tf
@@ -1,32 +1,9 @@
 locals {
-  clickhouse_sa_roles = {
-    compute_network_admin = {
-      # Manage VPC
-      role = "roles/compute.networkAdmin"
-    }
-    iam_service_account_admin = {
-      # Manage SA
-      role = "roles/iam.serviceAccountAdmin"
-    }
-    iam_service_account_key_admin = {
-      # Manage SA keys
-      role = "roles/iam.serviceAccountKeyAdmin"
-    }
-    iam_service_account_user = {
-      # We need iam.serviceAccounts.actAs permission to deploy GKE
-      role = "roles/iam.serviceAccountUser"
-    }
-    storage_admin = {
-      # Manage storage
-      role = "roles/storage.admin"
-    }
-    container_admin = {
-      # Manage GKE
-      role = "roles/container.admin"
-    }
-    resourcemanager_project_iam_admin = {
-      # Required to grant roles/container.defaultNodeServiceAccount to nodePool SA
-      role = "roles/resourcemanager.projectIamAdmin"
-    }
-  }
+  clickhouse_custom_roles = [
+    google_project_iam_custom_role.clickhouse_common_role.id,
+    google_project_iam_custom_role.clickhouse_vpc_role.id,
+    google_project_iam_custom_role.clickhouse_cluster_role.id,
+    google_project_iam_custom_role.clickhouse_storage_role.id,
+    google_project_iam_custom_role.clickhouse_iam_role.id,
+  ]
 }
