c9aa2898da1a1c2a640bbfe34d175d4cc3a9d789
Konstantin Plis <plis.konstantin@gmail.com>
2026-02-05 14:35:59 +0800

    chore(azure): formatting

--- modules/azure/locals.tf
+++ modules/azure/locals.tf
@@ -6,7 +6,7 @@ locals {
   # https://learn.microsoft.com/en-us/graph/permissions-reference
   graph_permissions = {
     "Application.ReadWrite.All" = "1bfefb4e-e0b5-418b-a88f-73c46d2cc8e9"
-    "User.Invite.All" = "09850681-111b-4a89-9bed-3f2cae46d706"
-    "User.ReadWrite.All" = "741f803b-c850-494e-b5df-cde7c675a1ca"
+    "User.Invite.All"           = "09850681-111b-4a89-9bed-3f2cae46d706"
+    "User.ReadWrite.All"        = "741f803b-c850-494e-b5df-cde7c675a1ca"
   }
 }
